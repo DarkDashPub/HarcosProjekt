8fffdd1f6c2d0538db757519add440fc5f4b5046
DarkDashPub <terjeinorbert2000@gmail.com>
2020-09-29 11:52:59 +0200

    Tapasztalat és Életerő beállítások módosítása

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -13,13 +13,65 @@ namespace HarcosProjekt
         private int alapEletero;
         private int alapSebzes;
 
-        public string Nev { get => nev; set => nev = value; }
-        public int Szint { get => szint; set => szint = value; }
-        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
-        public int Eletero { get => eletero; set => eletero = value; }
-        public int AlapEletero { get => alapEletero; set => alapEletero = value; }
-        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }
+        public String getNev() 
+        {
+            return this.nev;
+        }
 
+        public void setNev(String ujNev) 
+        {
+            this.nev = ujNev;
+        }
+
+        public int getSzint() 
+        {
+            return this.szint;
+        }
+
+        public void setSzint(int ujSzint) 
+        {
+            this.szint = ujSzint;
+        }
+
+        public int getTapasztalat() 
+        {
+            return this.tapasztalat;
+        }
+
+        public void setTapasztalat(int ertek) 
+        {
+            this.tapasztalat += ertek;
+            if (this.tapasztalat == SzintLepeshez || this.tapasztalat > SzintLepeshez) 
+            {
+                setSzint(getSzint()+1);
+                setEletero(MaxEletero);
+            }
+        }
+
+        public int getAlapEletero() 
+        {
+            return this.alapEletero;   
+        }
+
+        public int getAlapSebzes() 
+        {
+            return this.alapSebzes;
+        }
+
+        public int getEletero() 
+        {
+            return this.eletero;
+        }
+
+        public void setEletero(int ertek) 
+        {
+            this.eletero = ertek;
+            if (this.eletero < 1) 
+            {
+                setTapasztalat(0);
+            }
+        }
+            
         public int Sebzes { get => alapSebzes + szint; }
 
         public int MaxEletero { get => alapEletero + szint * 3; }
@@ -50,6 +102,56 @@ namespace HarcosProjekt
 
             eletero = MaxEletero;
 
+        }
+
+        public void Megkuzd(Harcos kihivo,Harcos kihivott) 
+        {
+            if (kihivo == kihivott)
+            {
+                Console.WriteLine("A harcos nem támadhatja meg saját magát!");
+            }
+            else if (kihivo.getEletero() == 0 || kihivott.getEletero() == 0) 
+            {
+                Console.WriteLine("A harc nem lehetséges,ugyanis az egyik harcos halott.");
+            }
+
+            kihivott.setEletero(kihivott.getEletero() - kihivo.Sebzes);
+
+            if (kihivott.getEletero() > 0) 
+            {
+                kihivo.setEletero(kihivo.getEletero() - kihivott.Sebzes);
+            }
+
+            if (kihivott.getEletero() < 1) 
+            {
+                kihivo.setTapasztalat(kihivo.getTapasztalat()+15);
+            }
+            else if(kihivo.getEletero() < 1) 
+            {
+                kihivott.setTapasztalat(kihivott.getTapasztalat()+15);
+            }
+            else if(kihivo.getEletero() > 0 && kihivott.getEletero() > 0) 
+            {
+                kihivo.setTapasztalat(kihivo.getTapasztalat()+5);
+                kihivott.setTapasztalat(kihivott.getTapasztalat() +5);
+            }
+        }
+
+        public void Gyogyul(Harcos gyogyitandoHarcos) 
+        {
+            if (gyogyitandoHarcos.getEletero() < 1)
+            {
+                gyogyitandoHarcos.setEletero(MaxEletero);
+            }
+            else 
+            {
+                gyogyitandoHarcos.setEletero(gyogyitandoHarcos.getEletero()+(3+szint));
+            }
+
+            if (gyogyitandoHarcos.getEletero() > MaxEletero) 
+            {
+                gyogyitandoHarcos.setEletero(MaxEletero);
+            }
         }
 
         public override string ToString()
